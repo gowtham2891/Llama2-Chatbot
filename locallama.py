f82c3b53e4788c5f5dbc823abf0cab638ed9a26a
Dupati Ganesh Gowtham <118389681+gowtham2891@users.noreply.github.com>
2024-06-27 14:43:10 +0530

    Add files via upload

---
+++ locallama.py
@@ -0,0 +1,36 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+
+## prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    
+    [
+        ("system", "You are an helpful assistant. Please response to the user queries"),
+        ("user","Question:{question}")
+    ]
+)
+
+st.title("llama2 Demo")
+input_text = st.text_input("Search the topic...")
+
+
+#ollama llama2 LLM
+
+llm = Ollama(model = 'llama2')
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
